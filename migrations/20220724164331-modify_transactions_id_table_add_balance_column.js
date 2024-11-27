80d565731c4ededf1dc4fdafea6336c37ba34d59
blackhole <blackhole@blackhole.blackhole>
2024-11-28 01:45:38 +0500

    changed all migration files

--- migrations/20220724164331-modify_transactions_id_table_add_balance_column.js
+++ migrations/20220724164331-modify_transactions_id_table_add_balance_column.js
@@ -9,6 +9,6 @@ module.exports = {
     );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('transactionids','amount');
+    //await queryInterface.removeColumn('transactionids','amount');
   }
 };
